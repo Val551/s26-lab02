45d2c12a40295e2196068ab1eef7e65bf682f52a
Fabio Campos <fabiocam@andrew.cmu.edu>
2026-01-21 21:50:19 -0500

    Add encapsulation for java

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
@@ -1,12 +1,15 @@
 package edu.cmu.cs.cs214.lab02;
 
+import edu.cmu.cs.cs214.lab02.shapes.Circle;
 import edu.cmu.cs.cs214.lab02.shapes.Rectangle;
+import edu.cmu.cs.cs214.lab02.shapes.Shape;
+import edu.cmu.cs.cs214.lab02.shapes.Square;
 
 public class Main {
     public static void main(String[] args) {
-        Rectangle rectangle = new Rectangle(2, 3);
-
-        Renderer renderer = new Renderer(rectangle);
+        
+        Shape shape = new Square(2);
+        Renderer renderer = new Renderer(shape);
 
         renderer.draw();
     }
